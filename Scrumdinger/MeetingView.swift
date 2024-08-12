2c39589c8b45fafcfbfbcb0dbd1c4ed6874fba1c
ariokusuma <ariopuboyo@gmail.com>
2024-08-12 21:57:21 +0700

    day1 done

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,17 +11,42 @@ import SwiftData
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello World")
+            ProgressView(value: 10, total: 15)
+//            Top Nav
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time Remaning")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+
+//            Bottom Nav
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
         }
         .padding()
-        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View{
         MeetingView()
     }
--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,16 @@
 //  Created by Ario Kusuma on 12/08/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self()}
+}
